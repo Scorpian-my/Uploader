59e09a04d92ed7f6b34a2f72e7a59bd433c91a88
Mahyar - Mortezaei <96655803+Scorpian-my@users.noreply.github.com>
2023-01-26 11:59:19 +0330

    Delete bot.py

--- bot.py
+++
@@ -1,9 +0,0 @@
-from rubpy import Client 
-from os import system 
-url = input("Enter Your Link: ") 
-regex = url[15:] 
-system(f"wget {url} && mv ./{regex} ./FreeTube.mp4") 
-app = Client("xdiwehhjsizxtgawudacczfqoegmcjyc") 
-async def send(app): 
-    await app.sendFile("c0BTbEE06bf77d2a9635c01bcaf13afc","FreeTube.mp4","مربوط به پست بالا 👆") 
-app.run(send)
